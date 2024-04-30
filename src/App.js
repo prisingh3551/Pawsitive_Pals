8cefb323b8cef498c009b89ea32cecf27270ee55
Priya Singh <99465484+prisingh3551@users.noreply.github.com>
2024-05-01 00:48:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter as  Router, Routes, Route} from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+import LoginSignup from './pages/LoginSignup';
 import Quiz from './pages/Quiz';
 import Donate from './pages/Donate';
 import ADOPTFORM from './pages/ADOPTFORM';
@@ -17,15 +16,13 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/loginsignup" element={<LoginSignup />} />
           <Route path="/quiz" element={<Quiz />} />
           <Route path="/donate" element={<Donate />} />
            <Route path="/adoptform" element={<ADOPTFORM />} />
            <Route path="/rehome" element={<Rehome />} />
            <Route path="/catspage" element={<CatsPage />} />
            <Route path="/dogspage" element={<DogsPage />} />
-           {/* <Route path="/adoptFill-form" element={<AdoptFillForm />} /> */}
          </Routes>
        </div>
     </Router>
